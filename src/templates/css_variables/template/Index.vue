6de52c4f36bf5a1b477b9bfdd381a520510b3262
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2024-06-21 15:09:57 +0200

    add missing sandboxes

--- src/templates/css_variables/template/Index.vue
+++ src/templates/css_variables/template/Index.vue
@@ -3,13 +3,40 @@ import GalaxyPanelLayout from "@/components/galaxy/GalaxyPanelLayout.vue";
 </script>
 
 <template>
-	<GalaxyPanelLayout>
+	<GalaxyPanelLayout class="main">
 		<h1>Custom CSS Parameters, or CSS Variables</h1>
+		<input class="position-slider" type="range" />
+		<div class="sliding-box-container">
+			<div class="sliding-box"></div>
+		</div>
 	</GalaxyPanelLayout>
 </template>
 
 <style scoped>
 .main {
-	--box-color: rgb(111, 201, 111);
+	--box-color-from: rgb(111, 201, 111);
+	--box-color-to: rgb(255, 121, 121);
+}
+
+.sliding-box-container {
+	border: 1px solid var(--gx-brand-dark);
+	height: 50px;
+	position: relative;
+	border-radius: 10px;
+	overflow: hidden;
+}
+
+.sliding-box {
+	position: relative;
+	left: calc(100% * 0 - 50px * 0);
+
+	width: 50px;
+	height: 50px;
+
+	background-color: color-mix(
+		in oklch longer hue,
+		var(--box-color-to) 0%,
+		var(--box-color-from)
+	);
 }
 </style>
